b28864add9dcf1e8379d4ed051216dddcda8270c
sambineroloc <sambineroloc@gmail.com>
2018-08-18 14:09:38 -0400

    Correct json error, write new method

--- app/models/model_constructor.rb
+++ app/models/model_constructor.rb
@@ -3,13 +3,18 @@ require 'singleton'
 class ModelConstructor
   include Singleton
 
-  attr_accessor :data
+  attr_accessor :data, :name_map
 
   def initialize
     @data = retrieve_json
+    @name_map = retrieve_name_map
   end
 
   def retrieve_json
     JSON.parse(File.read('public/assets/liber777.json'))
+  end
+
+  def retrieve_name_map
+    JSON.parse(File.read('public/assets/column_and_row_map.json'))
   end
 end
