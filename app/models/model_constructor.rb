cd09072ab94d0355816683ae04074322ce680e72
sambineroloc <sambineroloc@gmail.com>
2018-08-18 14:20:39 -0400

    Create method to create columns iteratively

--- app/models/model_constructor.rb
+++ app/models/model_constructor.rb
@@ -17,4 +17,12 @@ class ModelConstructor
   def retrieve_name_map
     JSON.parse(File.read('public/assets/column_and_row_map.json'))
   end
+
+  def create_columns
+    @data.each do |key, value|
+      title = value['columnName']
+      roman_numeral = key
+      Column.create(title: title, roman_numeral: roman_numeral)
+    end
+  end
 end
