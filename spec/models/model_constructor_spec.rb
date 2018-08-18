200dc32463c97d11105e439e62c49f31b38ee241
sambineroloc <sambineroloc@gmail.com>
2018-08-18 15:04:02 -0400

    Fix rubocop mistakes

--- spec/models/model_constructor_spec.rb
+++ spec/models/model_constructor_spec.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
 RSpec.describe ModelConstructor, type: :model do
+  let(:json) { JSON.parse(File.read('public/assets/column_and_row_map.json')) }
+  let(:name_map) do
+    JSON.parse(File.read('public/assets/column_and_row_map.json'))
+  end
   subject { described_class.instance }
 
   context 'when initialized' do
     it 'correctly returns JSON' do
-      expect(subject.data).to eq(
-        JSON.parse(
-          File.read('public/assets/liber777.json')
-        )
-      )
+      expect(subject.data).to eq(json)
     end
 
     it 'correctly loads the name map' do
-      expect(subject.name_map).to eq(
-        JSON.parse(
-          File.read('public/assets/column_and_row_map.json')
-        )
-      )
+      expect(subject.name_map).to eq(name_map)
+    end
+  end
+
+  context 'when using its methods' do
+    it 'makes columns correctly' do
+      subject.create_columns
+      expect(Column.all.length).to eq(json.keys.length)
     end
   end
 end
