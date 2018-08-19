effc4e0f6b60ef72b4de690b3266811f3bd3784e
sambineroloc <sambineroloc@gmail.com>
2018-08-18 20:16:29 -0400

    Minor change

--- app/controllers/rows_controller.rb
+++ app/controllers/rows_controller.rb
@@ -10,7 +10,7 @@ class RowsController < ApplicationController
   def show
     if params[:column]
       row = Row.find_by(query_param: params[:id].to_i)
-      column = Column.find_by(query_param:  params[:column])
+      column = Column.find_by(query_param: params[:column])
       @row = row.correspondences.find_by(column: column)
     else
       @row = Row.find_by(query_param: params[:id].to_i)
